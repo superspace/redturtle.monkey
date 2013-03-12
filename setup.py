ccef123e772d8699cf66bc2817906e645bbaf325
Andrew Mleczko <andrew@mleczko.net>
2013-03-12 17:34:47 +0100

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.1'
+version = '1.2.dev0'
 description = 'mailchimp integration for Plone.'
 long_description = \
     open("README.rst").read() + "\n" + \
