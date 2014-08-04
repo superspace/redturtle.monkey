f3ca21aec95c3d83cde37df5544fb62a62ed59da
Luca Fabbri <luca@keul.it>
2014-08-04 16:35:08 +0200

    fixed typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 version = '1.2.2.dev0'
-description = 'mailchimp integration for Plone.'
+description = 'MailChimp integration for Plone.'
 long_description = \
     open("README.rst").read() + "\n" + \
     open("CHANGES.rst").read()
