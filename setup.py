d262d5da304f58fa2730bc5ef8490147290068bb
Luca Fabbri <luca@keul.it>
2014-08-04 16:28:14 +0200

    Preparing release 1.2.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.2.1.dev0'
+version = '1.2.1'
 description = 'mailchimp integration for Plone.'
 long_description = \
     open("README.rst").read() + "\n" + \
